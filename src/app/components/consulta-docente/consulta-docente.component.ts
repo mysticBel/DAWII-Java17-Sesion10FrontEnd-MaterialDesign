7b28165cefb1c93cabcd3a3f43fc784207ef1213
mysticBel <dnxbel@gmail.com>
2023-11-19 09:19:34 -0500

    se llena los combitos

--- src/app/components/consulta-docente/consulta-docente.component.ts
+++ src/app/components/consulta-docente/consulta-docente.component.ts
@@ -1,5 +1,8 @@
 import { Component, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
+import { Ubigeo } from 'src/app/models/ubigeo.model';
+import { DocenteService } from 'src/app/services/docente.service';
+import { UbigeoService } from 'src/app/services/ubigeo.service';
 
 @Component({
   selector: 'app-consulta-docente',
@@ -15,5 +18,47 @@ dataSource:any;
 
 //Cabecera
 displayedColumns = ["idDocente","nombre","dni","fecha","hora","estado","ubigeo"];
+
+
+  //parametros de la consulta - lo seteamos a nulos
+  nombre:string  = "";
+  dni:string  = "";
+  estado:boolean  = true;
+  selDepartamento:string = "-1";
+  selProvincia:string = "-1";
+  selDistrito:number = -1;
+
+  //Para el ubigeo ( de los combitos)
+  departamentos: string[] = [];;
+  provincias: string[] = [];;
+  distritos: Ubigeo[] = [];;
+
+  // Consntructor para llenar combito
+  constructor(private docenteService:DocenteService, 
+              private  ubigeoService: UbigeoService){
+        //lenamos el combito de departamento
+        this.ubigeoService.listarDepartamento().subscribe(
+            x =>  this.departamentos = x
+        );
+
+    }
+
+    // Cargamos la provincia y los distritos del Ubigeo
+    cargaProvincia(){
+      this.ubigeoService.listaProvincias(this.selDepartamento).subscribe(
+        response =>  this.provincias= response
+      );
+      this.distritos = [];
+      this.selDistrito = -1;
+      this.selProvincia = "-1";
+   }
+  
+    cargaDistrito(){
+      this.ubigeoService.listaDistritos(this.selDepartamento, this.selProvincia).subscribe(
+          response =>  this.distritos= response
+      );
+      this.selDistrito = -1;
+  }
+  
 
 }
